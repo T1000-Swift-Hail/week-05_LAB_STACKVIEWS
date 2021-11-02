096504908a5b197b0bfbf3256b36696cb65b776f
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-11-02 13:05:13 +0300

    done

---
+++ LAB_STACKVIEWS/LAB_STACKVIEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_STACKVIEWS
+//
+//  Created by AryafAlaqabali on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

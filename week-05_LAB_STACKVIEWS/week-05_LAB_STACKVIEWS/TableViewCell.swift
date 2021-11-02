32b70eb82f8ac9b54ae1de9ffe8288bf29459f9e
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-11-02 14:38:53 +0300

    Done

---
+++ week-05_LAB_STACKVIEWS/week-05_LAB_STACKVIEWS/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  tableViewCell.swift
+//  week-05_LAB_STACKVIEWS
+//
+//  Created by mona aleid on 27/03/1443 AH.
+//
+
+import UIKit
+
+class tableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ week-05_LAB_STACKVIEWS/week-05_LAB_STACKVIEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-05_LAB_STACKVIEWS
+//
+//  Created by mona aleid on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

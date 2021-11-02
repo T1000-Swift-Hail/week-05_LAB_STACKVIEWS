b61c0cced3025342c017d6feac86496b2442940e
Anas,Majed,Abdullah <Anas.h.Alhumaid@gmail.com>
2021-11-02 13:42:38 +0300

    Slovakia info app

---
+++ Slovakia/Slovakia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slovakia
+//
+//  Created by Anas Hamad on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

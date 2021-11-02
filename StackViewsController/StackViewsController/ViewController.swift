a92d6fb714d08feceb42561a94f225eea3c5fb43
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-11-02 16:03:29 +0300

    The LAB StackView it's Done

---
+++ StackViewsController/StackViewsController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewsController
+//
+//  Created by MACBOOK on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

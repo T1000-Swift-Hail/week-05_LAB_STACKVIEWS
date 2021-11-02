4766f9cb46d7b3b2e8ada9738b51f80c6b2212e2
iAbdullah17 <user@MacBook-Air-.local>
2021-11-02 14:51:13 +0300

    Done (edit)

---
+++ Stack/Stack/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Stack
+//
+//  Created by iAbdullah17 on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var cityName: UILabel!
+    
+    @IBOutlet weak var cityDiscreabtion: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func learnMore(_ sender: Any) {
+        
+    }
+    
+}
+

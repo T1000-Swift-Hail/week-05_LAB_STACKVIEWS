3c2cd3ea21b8bd2c54ee20e7ce4f703df2c31ad6
SehamFulayyih <Seham.Fulaih.has.s@tuwaiq.edu.sa>
2021-11-02 13:13:48 +0300

    donelap

---
+++ Stack Views lap/Stack Views lap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack Views lap
+//
+//  Created by Seham الشطنان on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c8e293abcecd070c92f2a3dcc42b721bc5c98ce6
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-11-02 13:39:07 +0300

    Done HW

---
+++ LAB_STACKVIEWS/LAB_STACKVIEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_STACKVIEWS
+//
+//  Created by Munira abdullah on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LAB_STACKVIEWS/LAB_STACKVIEWSTests/LAB_STACKVIEWSTests.swift
@@ -0,0 +1,33 @@
+//
+//  LAB_STACKVIEWSTests.swift
+//  LAB_STACKVIEWSTests
+//
+//  Created by Munira abdullah on 27/03/1443 AH.
+//
+
+import XCTest
+@testable import LAB_STACKVIEWS
+
+class LAB_STACKVIEWSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

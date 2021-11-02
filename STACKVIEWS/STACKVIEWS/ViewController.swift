b947bb6bf52e91ab4fed955a3d5941f46ed01eb8
Monafh <name@gmail.com>
2021-11-02 12:41:06 +0300

    OK

---
+++ STACKVIEWS/STACKVIEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STACKVIEWS
+//
+//  Created by Monafh on 27/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

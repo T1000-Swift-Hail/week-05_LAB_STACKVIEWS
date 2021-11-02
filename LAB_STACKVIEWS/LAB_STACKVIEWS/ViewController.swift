24760e1a0524be0b17ba162962f516243e436af4
azoz <aabdoalaziz1@gmail.com>
2021-11-02 15:23:09 +0300

    HW_labviews is Done

---
+++ LAB_STACKVIEWS/LAB_STACKVIEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_STACKVIEWS
+//
+//  Created by Mac on 02/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
